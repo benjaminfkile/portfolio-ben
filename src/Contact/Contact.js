6619caa273712e9c537ea362b4c7c7c1a95cf3a8
ben <benjaminfkile@gmail.com>
2020-03-02 23:52:26 -0700

    portfolio is now full stack

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -4,57 +4,142 @@ import '../Contact/Contact.css'
 
 class Contact extends Component {
 
-    handleSubmit(e){
+    state = {
+        formFilled: true,
+        success: false,
+        failure: false,
+        thanks: false,
+        send: true,
+        everythingElse: true
+    }
+
+
+    handleSuccess() {
+        setTimeout(
+            function () {
+                this.setState(
+                    {
+                        success: false,
+                        send: true
+                    }
+                );
+                // this.reRoute()
+            }
+                .bind(this),
+            3000
+        );
+
+    }
+
+    handleFormFilled() {
+        setTimeout(
+            function () {
+                this.setState(
+                    {
+                        formFilled: true,
+                        send: true
+                    }
+                );
+            }
+                .bind(this),
+            3000
+        );
+    }
+
+    handleSubmit(e) {
         e.preventDefault();
+        this.setState({ send: false })
+        this.setState({ failure: false });
         const name = document.getElementById('name').value;
         const email = document.getElementById('email').value;
         const message = document.getElementById('message').value;
-        axios({
-            method: "POST", 
-            url:"http://localhost:3002/send", 
-            data: {
-                name: name,   
-                email: email,  
-                message: message
-            }
-        }).then((response)=>{
-            if (response.data.msg === 'success'){
-                alert("Message Sent."); 
-                this.resetForm()
-            }else if(response.data.msg === 'fail'){
-                alert("Message failed to send.")
-            }
-        })
-        console.log("Name: " + name)
-        console.log("Email: " + email)
-        console.log("Message: " + message)
+        if (name && email && message) {
+            axios({
+                method: "POST",
+                url: "https://rocky-lowlands-64636.herokuapp.com/send",
+                data: {
+                    name: name,
+                    email: email,
+                    message: message
+                }
+            }).then((response) => {
+                if (response.data.msg === 'success') {
+                    console.log("Message Sent.");
+                    this.setState({ success: true })
+                    this.resetForm()
+                    this.handleSuccess()
+                } else if (response.data.msg === 'fail') {
+                    console.log("Message failed to send.")
+                    this.setState({ failure: true })
+                }
+            })
+        }
+        else {
+            this.setState({ formFilled: false })
+            this.handleFormFilled()
+        }
     }
 
-    resetForm(){
-        document.getElementById('contact-form').reset();
+    resetForm() {
+        document.getElementById('Main_Form').reset();
     }
 
-    render(){
-        return(
+    render() {
+        return (
+
             <div className="Contact">
-                <form id="Main_Form" onSubmit={this.handleSubmit.bind(this)} method="POST">
-                    <div className="Name_Form">
-                        <label for="name">Name:</label>
+                {this.state.everythingElse && <form id="Main_Form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+                    <div className="Name_Email">
+                        <h1>
+                            Say Hello
+                        </h1>
+                        <div className="Name_Form">
+                            <label for="name">Name*</label>
+                            <br></br>
+                            <input type="text" className="form-control" id="name" />
+                        </div>
                         <br></br>
-                        <input type="text" className="form-control" id="name" />
-                    </div>
-                    <div className="Email_Form">
-                        <label for="exampleInputEmail1">Email:</label>
                         <br></br>
-                        <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
+                        <div className="Email_Form">
+                            <label for="exampleInputEmail1">Email*</label>
+                            <br></br>
+                            <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
+                        </div>
                     </div>
-                    <div className="Message_Form">
-                        <label for="message">Message:</label>
+                    <div className="Message">
+                        <div className="Message_Form">
+                            <h2>
+                                Message*
+                            </h2>
+                            <br></br>
+                            <textarea className="form-control" rows="5" id="message"></textarea>
+                        </div>
+                        {this.state.send && <img id="Submit_Btn" src="./res/mail.png" alt="Send" onClick={this.handleSubmit.bind(this)} method="POST" height="42" width="42" />}
+
+                    </div>
+
+                </form>}
+                {this.state.success && <div className="Success">
+                    <h1>
+                        thanks,  your message was sent!!!
+                    </h1>
+                </div>}
+                {this.state.failure && <div className="Failure">
+                    <h1>
+                        well.. this is embarrasing :(
+                    </h1>
+                    <p>
+                        try messaging me here instead
                         <br></br>
-                        <textarea className="form-control" rows="5" id="message"></textarea>
-                    </div>
-                    <button type="submit" className="Submit_Btn">Submit</button>
-                </form>
+                    </p>
+                    <br></br>
+                    <a href="mailto:benjaminfkile@gmail.com">Ben Kile</a>
+                </div>}
+                {!this.state.formFilled && <div className="Incomplete_Form">
+                    <h2>
+                        don't leave me hangin, fill out all the fields
+                        </h2>
+                </div>}
             </div>
         )
     }
