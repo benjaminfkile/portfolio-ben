a40a31979e117bee27eb925cde262dbb83505efe
ben <benjaminfkile@gmail.com>
2020-03-03 19:14:22 -0700

    Updates

---
+++ precache-manifest.e34e18451bd53a10da8d67e2f19e62a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24b6c2ca93fe4306d74204f08a3442a1",
+    "url": "/portfolio-ben/index.html"
+  },
+  {
+    "revision": "c2ce45f7ff3d31987232",
+    "url": "/portfolio-ben/static/css/main.d3b40292.chunk.css"
+  },
+  {
+    "revision": "fc8446589904355cea85",
+    "url": "/portfolio-ben/static/js/2.8f0cbd95.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio-ben/static/js/2.8f0cbd95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2ce45f7ff3d31987232",
+    "url": "/portfolio-ben/static/js/main.bd68feb4.chunk.js"
+  },
+  {
+    "revision": "bacd02882e3f27b92350",
+    "url": "/portfolio-ben/static/js/runtime-main.77919a00.js"
+  }
+]);
