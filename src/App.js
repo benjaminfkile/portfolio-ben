f4495cb9d6b6b8225eacaf20f26fd71b8cd1bba0
ben <benjaminfkile@gmail.com>
2020-03-12 06:06:20 -0600

    wake up heroku as soon as portfolio in visited to speed u mail times

--- src/App.js
+++ src/App.js
@@ -6,9 +6,29 @@ import About from './About/About'
 import Skills from './Skills/Skills'
 import Projects from './Projects/Projects'
 import Contact from './Contact/Contact'
+import axios from 'axios';
+
 
 import './App.css';
 function App() {
+
+  axios({
+    method: "POST",
+    url: "https://rocky-lowlands-64636.herokuapp.com/send",
+    data: {
+        name: "portfolio alarm",
+        email: "benjaminfkile@gmail.com",
+        message: "portfolio visit!!!"
+    }
+}).then((response) => {
+    if (response.data.msg === 'success') {
+        console.log("Message Sent.");
+    } else if (response.data.msg === 'fail') {
+        console.log("Message failed to send.")
+    }
+})
+
+
   return (
     <div className="App">
       <Nav />
